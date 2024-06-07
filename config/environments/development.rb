e8027713dabbb4ad555c616bf73766cf2ca4c730
tomoki04110610 <161846373+tomoki04110610@users.noreply.github.com>
2024-06-07 00:10:42 +0000

    サーバー立ち上げ　完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "acae089417f3452ca27ff3a9d5a4e2ea"
+  config.hosts.clear
+
 end
+
+
