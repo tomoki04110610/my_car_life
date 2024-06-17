7847cbffb0a2f03087f22f504eb11a1df6ed94fc
tomoki04110610 <161846373+tomoki04110610@users.noreply.github.com>
2024-06-17 07:40:59 +0000

    Postバリデーション完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(post_params)
     if @post.save
-      redirect_to post_path(post.id)
+      redirect_to post_path(@post.id)
     else
       render :new
     end
@@ -25,9 +25,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post.id)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+       redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
