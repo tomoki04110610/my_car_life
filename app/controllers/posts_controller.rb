9ec435e86d4c95b1b577d0bddb128caeef77317a
tomoki04110610 <161846373+tomoki04110610@users.noreply.github.com>
2024-06-24 05:15:42 +0000

    post post_image投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :body, :genre_id)
+    params.require(:post).permit(:title, :body, :genre_id, :post_image)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,14 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+
+  has_one_attached :post_image
+
+  def get_post_image(width, height)
+    unless post_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    post_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
