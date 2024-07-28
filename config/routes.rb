e2fc69c8ce5fa0528293ceccc6e486067bccb01a
tomoki04110610 <161846373+tomoki04110610@users.noreply.github.com>
2024-07-28 13:52:34 +0000

    ログアウトエラー修正完了　管理者機能削除実装途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,28 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :users, only: [:show, :index, :edit, :update, :destroy]
-  get '/mypage', to: 'users#mypage', as: 'mypage'
+  get "/mypage", to: "users#mypage", as: "mypage"
   resources :posts
     resources :post_comments, only: [:create, :destroy]
   resources :car_models, only: [:new, :create, :edit, :update, :destroy]
   resources :driving_distances, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  get '/homes/about', to: 'homes#about', as: 'about'
+  get '/homes/about', to: "homes#about", as: "about"
   get "/search", to: "searches#search", as: "search"
 
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    delete "users/sign_out", to: "devise/sessions#destroy"
   end
 
-  devise_for :users
   devise_for :admin, skip: [:registrations, :password], controllers: {
-    sessions: 'admin/sessions'
+    sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
-    get 'dashboards', to:'dashboards#index'
+    get "dashboards", to:"dashboards#index"
     resources :users, only: [:destroy]
   end
 
