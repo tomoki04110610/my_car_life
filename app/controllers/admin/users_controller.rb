070a96f080924c3290c9bce37be1f82f3c927f8b
tomoki04110610 <161846373+tomoki04110610@users.noreply.github.com>
2024-08-26 01:37:41 +0000

    ﾍｯﾀﾞｰいいねリンク追加　管理者側ユーザー削除をソフトデリートに修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-    redirect_to admin_dashboards_path, notice: 'ユーザーを削除しました。'
+    @user.update(is_active: false)
+    redirect_to admin_dashboards_path, notice: 'ユーザーを無効化しました。'
   end
 end
