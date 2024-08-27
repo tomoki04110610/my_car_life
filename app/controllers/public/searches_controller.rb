fc5c88d95a73e2075aed0149d2a3f59f8d8c753c
tomoki04110610 <161846373+tomoki04110610@users.noreply.github.com>
2024-08-27 03:53:29 +0000

    レイアウト再調整　searchにページネーションを追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,15 +2,18 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
+    # 選択モデル
     @model = params[:model]
+    # 検索ワード
     @content = params[:content]
+    # 検索条件
     @method = params[:method]
 
     # 選択したモデルに応じて検索を実行
     if @model  == "user"
-      @records = User.search_for(@content, @method)
+      @records = User.search_for(@content, @method).page(params[:page])
     else
-      @records = Post.search_for(@content, @method)
+      @records = Post.search_for(@content, @method).page(params[:page])
     end
   end
 end
