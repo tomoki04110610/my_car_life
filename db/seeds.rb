88e8474e8cf2cff0f40cea7c00468ed7c30ea351
tomoki04110610 <161846373+tomoki04110610@users.noreply.github.com>
2024-08-28 01:28:36 +0000

    seedfile作成完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,10 @@
 
 # db/seeds.rb
 
-# ニックネームの候補をカタカナで用意
-nicknames = ["スピードスター", "ナビゲーター", "ロードランナー", "ギアヘッド", "ターボマスター",
-             "ロードキング", "ヴェローチェ", "ドリフトマスター", "タイヤバーナー", "オートグル"]
+
+# ニックネームの候補用意
+nicknames = ["ヘッドカバー", "シリンダーヘッド", "カムシャフト", "コンロッド", "クランクシャフト",
+             "ロッカーアーム", "シリンダーブロック", "オイルパン", "インテークマニホールド", "エキゾーストマニホールド"]
 
 # 車の名前の配列を用意
 car_names = ["スカイライン", "セドリック", "180sx", "シルビア", "オッティ", "ワゴンＲ", "エスティマ", "デミオ",
@@ -22,27 +23,32 @@ car_names = ["スカイライン", "セドリック", "180sx", "シルビア", "
 
 # ユーザーを10人作成し、それぞれに2台の車と走行距離を作成
 10.times do |n|
-  user = User.create!(
+  user = User.find_or_create_by(
     email: "user#{n+1}@example.com",
-    encrypted_password: BCrypt::Password.create('1234'),  # パスワードを簡単に
     last_name: "LastName#{n+1}",
     first_name: "FirstName#{n+1}",
     last_name_kana: "ラストネーム#{n+1}",
     first_name_kana: "ファーストネーム#{n+1}",
     nickname: nicknames[n % nicknames.length],
     is_active: true
-  )
+  ) do |u|
+    u.password = '123456'
+    u.password_confirmation = '123456'
+  end
 
   # 各ユーザーに2台の車を作成し、それぞれにランダムな走行距離を登録
   2.times do |i|
     car_name = car_names.shift
-    car = CarModel.create!(
+    car = CarModel.find_or_create_by(
       user_id: user.id,
       name: car_name
     )
 
+   # デフォルト値を作成
+    user.default_values.find_or_create_by(car_model_id: car.id)
+
     # ランダムな走行距離を生成して登録
-    DrivingDistance.create!(
+    DrivingDistance.find_or_create_by(
       user_id: user.id,
       car_model_id: car.id,
       distance: rand(100..150000)  # 100〜150,000の間でランダム生成
@@ -65,3 +71,9 @@ end
 
 puts "10 users with 2 cars each and their driving distances have been created."
 puts "Genres have been created or found."
+
+# 管理者アカウントの作成
+Admin.find_or_create_by!(email: 'admin@example.com') do |admin|
+  admin.password = 'password'
+  admin.password_confirmation = 'password'
+end
